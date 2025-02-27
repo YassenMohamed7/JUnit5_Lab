b746b8e979410c437b8820f8232e82cc0d0a4e55
YassenMohamed7 <yassenmohammed871@gmial.com>
2025-02-27 12:11:23 +0200

    add free ProductManagerTest class

---
+++ src/test/java/com/unit/testing/products/ProductManagementTest.java
@@ -0,0 +1,21 @@
+package com.unit.testing.products;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import java.util.stream.Stream;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+
+class ProductManagerTest {
+
+
+}
